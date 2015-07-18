c364da37881c38793d1019002bd2c33aa5fdf6f7
mousedevelop <937279156@163.com>
2015-07-18 15:20:00 +0800

    version:0.0.1
    Signed-off-by: mousedevelop <937279156@163.com>

---
+++ main/java/unstudio/chinacraft/ChinaCraft.java
@@ -0,0 +1,38 @@
+package unstudio.chinacraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ChinaCraft.MODID, version = ChinaCraft.VERSION)
+public class ChinaCraft {
+	    public static final String MODID = "chinacraft";
+	    public static final String VERSION = "0.0.1";
+	 
+	    @SidedProxy(clientSide = "unstudio.chinacraft.ClientProxy",
+	            serverSide = "unstudio.chinacraft.CommonProxy")
+	    public static CommonProxy proxy;
+	 
+	    @Instance("ChinaCraft")
+	    public static ChinaCraft instance;
+	 
+	    @EventHandler
+	    public void preInit(FMLPreInitializationEvent event) {
+	        proxy.preInit(event);
+	    }
+	 
+	    @EventHandler
+	    public void init(FMLInitializationEvent event) {
+	        proxy.init(event);
+	    }
+	 
+	    @EventHandler
+	    public void postInit(FMLPostInitializationEvent event) {
+	        proxy.postInit(event);
+	    }
+	    
+}
---
+++ main/java/unstudio/chinacraft/ClientProxy.java
@@ -0,0 +1,19 @@
+package unstudio.chinacraft;
+
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class ClientProxy extends CommonProxy { //必须要继承CommonProxy
+    public void preInit(FMLPreInitializationEvent event) {
+        //PreInit方法
+    }
+ 
+    public void init(FMLInitializationEvent event) {
+    //Init方法 进行注册物品 方块的模型 材质
+    }
+ 
+    public void postInit(FMLPostInitializationEvent event) {
+        //PostInit方法
+    }
+}
