0ec06d3ae43e7482b6ec5df346f81c8c1976f735
Mouse <937279156@163.com>
2015-07-18 15:58:15 +0800

    version:0.0.1.3

--- main/java/unstudio/chinacraft/ClientProxy.java
+++ main/java/unstudio/chinacraft/ClientProxy.java
@@ -6,14 +6,14 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy { 
     public void preInit(FMLPreInitializationEvent event) {
-        //PreInit方法
+    	
     }
  
     public void init(FMLInitializationEvent event) {
-    //Init方法 进行注册物品 方块的模型 材质
+    	
     }
  
     public void postInit(FMLPostInitializationEvent event) {
-        //PostInit方法
+    	
     }
 }
--- main/java/unstudio/chinacraft/CommonProxy.java
+++ main/java/unstudio/chinacraft/CommonProxy.java
@@ -6,14 +6,14 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
-    //PreInit方法 主要进行Config读取 设置等
+    	
     }
  
     public void init(FMLInitializationEvent event) {
-    //Init方法 主要进行设置方块 物品等
+    	
     }
  
     public void postInit(FMLPostInitializationEvent event) {
-    //PostInit方法
+
     }
 }
