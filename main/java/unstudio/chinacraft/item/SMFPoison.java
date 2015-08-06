aa7e52e85442d07e76475642d6a299b69c4dcbd6
trychen <trychen@live.com>
2015-08-06 10:39:49 +0800

    version 0.0.1.83

---
+++ main/java/unstudio/chinacraft/item/SMFPoison.java
@@ -0,0 +1,27 @@
+package unstudio.chinacraft.item;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+
+public class SMFPoison extends SpiritualMagicFigures{
+    public SMFPoison(){
+        setUnlocalizedName("spiritual_magic_figures_night_poison");
+    }
+    @Override
+    public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+    {
+        target.addPotionEffect(new PotionEffect(19,50));
+        stack.stackSize = stack.stackSize -1;
+        return false;
+    }
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
+    {
+        player.addPotionEffect(new PotionEffect(19, 50));
+        stack.stackSize = stack.stackSize -1;
+        return stack;
+    }
+}
