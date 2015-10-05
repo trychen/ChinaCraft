145a8e54e2eef7955578fd1b749b743b2af5e3ec
trychen <trychen@live.com>
2015-10-06 06:20:50 +0800

    version 0.0.1.50X

--- main/java/unstudio/chinacraft/item/SMFSuper.java
+++ main/java/unstudio/chinacraft/item/SMFSuper.java
@@ -3,10 +3,13 @@ package unstudio.chinacraft.item;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
-import net.minecraft.entity.ai.EntityAIWander;
-import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.monster.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SMFSuper extends SpiritualMagicFigures{
     public SMFSuper(){
@@ -15,11 +18,61 @@ public class SMFSuper extends SpiritualMagicFigures{
 
     @Override
     public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-        if (target instanceof EntityZombie){
-            EntityZombie entityZombie = (EntityZombie) target;
-            entityZombie.tasks.removeTask(new EntityAIAttackOnCollide(entityZombie, EntityPlayer.class, 1.0D, false));
-            entityZombie.tasks.removeTask(new EntityAIWander(entityZombie, 1.0D));
-            entityZombie.targetTasks.removeTask( new EntityAINearestAttackableTarget(entityZombie, EntityPlayer.class, 0, true));
+        if (!attacker.worldObj.isRemote) {
+//            if (target instanceof EntityCreeper) {
+//                final EntityCreature creeper = (EntityCreature) target;
+//                creeper.tasks.addTask(0, new EntityAIAttackOnCollide(creeper, EntityMob.class, 1.0D, false));
+//                creeper.targetTasks.addTask(0, new EntityAINearestAttackableTarget(creeper, EntityMob.class, 0, true));
+//            } else if (target instanceof EntityZombie){
+//                final EntityZombie zombie = (EntityZombie) target;
+//                zombie.tasks.addTask(0, new EntityAIAttackOnCollide(zombie, EntityMob.class, 1.0D, false));
+//                zombie.targetTasks.addTask(0, new EntityAINearestAttackableTarget(zombie, EntityMob.class, 0, true));
+//                new Timer().schedule(new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        zombie.setHealth(0);
+//                    }
+//                }, 15 * 1000);
+//            } else if (target instanceof EntityPlayer){
+//                target.addPotionEffect(new PotionEffect(16, 700));
+//            } else if (target instanceof EntitySkeleton){
+//                final EntitySkeleton skeleton = (EntitySkeleton) target;
+//                skeleton.tasks.addTask(0, new EntityAIAttackOnCollide(skeleton, EntityMob.class, 1.0D, false));
+//                skeleton.targetTasks.addTask(0, new EntityAINearestAttackableTarget(skeleton, EntityMob.class, 0, true));
+//                new Timer().schedule(new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        skeleton.setHealth(0);
+//                    }
+//                }, 15 * 1000);
+//            } else
+            if (target instanceof EntityMob){
+                final EntityMob mob = (EntityMob) target;
+                if (!(target instanceof EntitySkeleton)) {
+                    mob.tasks.addTask(0, new EntityAIAttackOnCollide(mob, EntityMob.class, 1.0D, false));
+                }
+                mob.targetTasks.addTask(0, new EntityAINearestAttackableTarget(mob, EntityMob.class, 0, true));
+                if (!(target instanceof EntityEnderman || target instanceof EntitySpider || target instanceof EntityCaveSpider)){
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            mob.setHealth(0);
+                        }
+                    }, 15 * 1000);
+                }
+            } else if(target instanceof EntityPlayer){
+                target.addPotionEffect(new PotionEffect(16, 700));
+            }
+
+        }
+
+
+        if (attacker instanceof EntityPlayer && target instanceof EntityMob) {
+            if (!((EntityPlayer) attacker).capabilities.isCreativeMode) {
+                if (!(target instanceof EntityEnderman || target instanceof EntitySpider || target instanceof EntityCaveSpider)){
+                    --stack.stackSize;
+                }
+            }
         }
         return false;
     }
--- main/java/unstudio/chinacraft/item/jade/Jade.java
+++ main/java/unstudio/chinacraft/item/jade/Jade.java
@@ -1,8 +1,20 @@
 package unstudio.chinacraft.item.jade;
 
+import java.util.List;
 import java.util.Random;
 
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.client.MinecraftForgeClient;
+import org.lwjgl.opengl.GL11;
 import unstudio.chinacraft.ChinaCraft;
 
 public class Jade extends Item{
@@ -28,5 +40,14 @@ public class Jade extends Item{
 	}
 	public int getlevel() {
 		return this.Level;
+	}
+	@Override
+	public void addInformation(ItemStack p_77624_1_, EntityPlayer p_77624_2_, List p_77624_3_, boolean p_77624_4_) {
+		p_77624_3_.add(StatCollector.translateToLocal("item.jade.lore") + this.getlevel());
+	}
+
+	public int getEnchantability()
+	{
+		return 3;
 	}
 }
