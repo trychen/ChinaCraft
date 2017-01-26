7105d5b8fc0db1db68a7f087f099bd390c07812e
Trychen <trychen@live.com>
2017-01-26 13:45:20 +0800

    Fixed Shield Listener

--- src/main/java/unstudio/chinacraft/event/combat/ListenerShield.java
+++ src/main/java/unstudio/chinacraft/event/combat/ListenerShield.java
@@ -17,63 +17,63 @@ import unstudio.chinacraft.item.CCShield;
  * Created by trychen on 16/10/7.
  */
 public class ListenerShield {
-    @SubscribeEvent(priority = EventPriority.LOW)
-    public void attackEvent(LivingHurtEvent e) {
-        if (!(e.entityLiving instanceof EntityPlayer)) {
-            return;
-        }
-        EntityPlayer player = (EntityPlayer) e.entityLiving;
-        if (player.getItemInUse() == null) {
-            return;
-        }
-        float damage = e.ammount;
-        ItemStack activeItemStack = player.getItemInUse();
-
-        if (damage > 0.0F && activeItemStack != null && activeItemStack.getItem() instanceof CCShield) {
-            int i = 1 + MathHelper.floor_float(damage);
-            activeItemStack.damageItem(i, player);
-
-            e.ammount = ((CCShield)activeItemStack.getItem()).defense(activeItemStack,player,e.source,e.ammount);
-
-            if (activeItemStack.stackSize <= 0) {
-                net.minecraftforge.event.ForgeEventFactory.onPlayerDestroyItem(player, activeItemStack);
-
-                player.setItemInUse(null,0);
-                if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
-                    player.playSound("entity.item.break", 0.8F, 0.8F + player.worldObj.rand.nextFloat() * 0.4F);
-                }
-            }
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOW)
-    public void attackEvent(LivingAttackEvent e) {
-        if (!(e.entityLiving instanceof EntityPlayer)) {
-            return;
-        }
-        EntityPlayer player = (EntityPlayer) e.entityLiving;
-        if (player.getItemInUse() == null) {
-            return;
-        }
-        float damage = e.ammount;
-        ItemStack activeItemStack = player.getItemInUse();
-
-        if (damage > 0.0F && activeItemStack != null && activeItemStack.getItem() instanceof CCShield) {
-            int i = 1 + MathHelper.floor_float(damage);
-            activeItemStack.damageItem(i, player);
-
-            damage = ((CCShield)activeItemStack.getItem()).defense(activeItemStack,player,e.source,e.ammount);
-
-            if (e.source.isProjectile() && damage == 0) {
-                e.setCanceled(true);
-                if (activeItemStack.stackSize <= 0) {
-                    net.minecraftforge.event.ForgeEventFactory.onPlayerDestroyItem(player, activeItemStack);
-                    player.setItemInUse(null,0);
-                    if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
-                        player.playSound("entity.item.break", 0.8F, 0.8F + player.worldObj.rand.nextFloat() * 0.4F);
-                    }
-                }
-            }
-        }
-    }
+//    @SubscribeEvent(priority = EventPriority.LOW)
+//    public void attackEvent(LivingHurtEvent e) {
+//        if (!(e.entityLiving instanceof EntityPlayer)) {
+//            return;
+//        }
+//        EntityPlayer player = (EntityPlayer) e.entityLiving;
+//        if (player.getItemInUse() == null) {
+//            return;
+//        }
+//        float damage = e.ammount;
+//        ItemStack activeItemStack = player.getItemInUse();
+//
+//        if (damage > 0.0F && activeItemStack != null && activeItemStack.getItem() instanceof CCShield) {
+//            int i = 1 + MathHelper.floor_float(damage);
+//            activeItemStack.damageItem(i, player);
+//
+//            e.ammount = ((CCShield)activeItemStack.getItem()).defense(activeItemStack,player,e.source,e.ammount);
+//
+//            if (activeItemStack.stackSize <= 0) {
+//                net.minecraftforge.event.ForgeEventFactory.onPlayerDestroyItem(player, activeItemStack);
+//
+//                player.setItemInUse(null,0);
+//                if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
+//                    player.playSound("entity.item.break", 0.8F, 0.8F + player.worldObj.rand.nextFloat() * 0.4F);
+//                }
+//            }
+//        }
+//    }
+//
+//    @SubscribeEvent(priority = EventPriority.LOW)
+//    public void attackEvent(LivingAttackEvent e) {
+//        if (!(e.entityLiving instanceof EntityPlayer)) {
+//            return;
+//        }
+//        EntityPlayer player = (EntityPlayer) e.entityLiving;
+//        if (player.getItemInUse() == null) {
+//            return;
+//        }
+//        float damage = e.ammount;
+//        ItemStack activeItemStack = player.getItemInUse();
+//
+//        if (damage > 0.0F && activeItemStack != null && activeItemStack.getItem() instanceof CCShield) {
+//            int i = 1 + MathHelper.floor_float(damage);
+//            activeItemStack.damageItem(i, player);
+//
+//            damage = ((CCShield)activeItemStack.getItem()).defense(activeItemStack,player,e.source,e.ammount);
+//
+//            if (e.source.isProjectile() && damage == 0) {
+//                e.setCanceled(true);
+//                if (activeItemStack.stackSize <= 0) {
+//                    net.minecraftforge.event.ForgeEventFactory.onPlayerDestroyItem(player, activeItemStack);
+//                    player.setItemInUse(null,0);
+//                    if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
+//                        player.playSound("entity.item.break", 0.8F, 0.8F + player.worldObj.rand.nextFloat() * 0.4F);
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
