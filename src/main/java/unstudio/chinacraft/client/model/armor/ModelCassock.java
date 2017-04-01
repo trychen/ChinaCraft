50ba9be237576d1e0ff507bbb3bc785ef3ed8aba
Mouse <937279156@qq.com>
2017-04-01 23:20:51 +0800

    Fixed render

--- src/main/java/unstudio/chinacraft/client/model/armor/ModelCassock.java
+++ src/main/java/unstudio/chinacraft/client/model/armor/ModelCassock.java
@@ -13,6 +13,8 @@ public class ModelCassock extends ModelBiped {
     ModelRenderer armorLeftArm;
     ModelRenderer armorOuterBody;
     ModelRenderer armorOuterLeftArm;
+    ModelRenderer armorOuterRightLeg;
+    ModelRenderer armorOuterLeftLeg;
 
     public ModelCassock() {
         super(0, 0, 256, 128);
@@ -21,69 +23,69 @@ public class ModelCassock extends ModelBiped {
 
         armorRightLeg = new ModelRenderer(this, 0, 16);
         armorRightLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
-        armorRightLeg.setRotationPoint(-2F, 12F, 0F);
+        //armorRightLeg.setRotationPoint(-2F, 12F, 0F);
         armorRightLeg.setTextureSize(256, 128);
         armorRightLeg.mirror = true;
 
 //        armorLeftLeg.mirror = true;
         armorLeftLeg = new ModelRenderer(this, 0, 16);
         armorLeftLeg.addBox(-2F, 0F, -2F, 4, 12, 4);
-        armorLeftLeg.setRotationPoint(2F, 12F, 0F);
+        //armorLeftLeg.setRotationPoint(2F, 12F, 0F);
         armorLeftLeg.setTextureSize(256, 128);
         armorLeftLeg.mirror = true;
 
 //        armorLeftLeg.mirror = false;
         armorInterBody = new ModelRenderer(this, 16, 16);
         armorInterBody.addBox(-4F, 0F, -2F, 8, 12, 4);
-        armorInterBody.setRotationPoint(0F, 0F, 0F);
+        //armorInterBody.setRotationPoint(0F, 0F, 0F);
         armorInterBody.setTextureSize(256, 128);
         armorInterBody.mirror = true;
 
         armorRightArm = new ModelRenderer(this, 40, 16);
         armorRightArm.addBox(-4F, 0F, -2F, 4, 11, 4);
-        armorRightArm.setRotationPoint(-4F, 0F, 0F);
+        //armorRightArm.setRotationPoint(-4F, 0F, 0F);
         armorRightArm.setTextureSize(256, 128);
         armorRightArm.mirror = true;
 
         armorLeftArm = new ModelRenderer(this, 40, 16);
         armorLeftArm.addBox(0F, 0F, -2F, 4, 11, 4);
-        armorLeftArm.setRotationPoint(4F, 0F, 0F);
+        //armorLeftArm.setRotationPoint(4F, 0F, 0F);
         armorLeftArm.setTextureSize(256, 128);
         armorLeftArm.mirror = true;
 
         armorOuterBody = new ModelRenderer(this, 0, 32);
         armorOuterBody.addBox(-4.5F, -0.5F, -2.5F, 9, 14, 5);
-        armorOuterBody.setRotationPoint(0F, 0F, 0F);
+        //armorOuterBody.setRotationPoint(0F, 0F, 0F);
         armorOuterBody.setTextureSize(256, 128);
         armorOuterBody.mirror = true;
 
         armorOuterLeftArm = new ModelRenderer(this, 36, 32);
         armorOuterLeftArm.addBox(-2.5F, -0.5F, -2.5F, 5, 11, 5);
-        armorOuterLeftArm.setRotationPoint(6F, 0F, 0F);
+        armorOuterLeftArm.setRotationPoint(0.75F, -1.625F, 0F);
         armorOuterLeftArm.setTextureSize(256, 128);
         armorOuterLeftArm.mirror = true;
 
-        bipedLeftArm.addChild(armorLeftArm);
-        bipedLeftArm.addChild(armorOuterLeftArm);
+        armorOuterRightLeg = new ModelRenderer(this, 0, 0);
+        armorOuterRightLeg.addBox(0F, 0F, 0F, 1, 1, 1);
+        //armorOuterRightLeg.setRotationPoint(2F, 12.5F, 0F);
+        armorOuterRightLeg.setTextureSize(256, 128);
+        armorOuterRightLeg.mirror = true;
 
+        armorOuterLeftLeg = new ModelRenderer(this, 0, 45);
+        armorOuterLeftLeg.addBox(-2.5F, 0F, -2.5F, 5, 10, 5);
+        //armorOuterLeftLeg.setRotationPoint(2F, 12.5F, 0F);
+        armorOuterLeftLeg.setTextureSize(256, 128);
+        armorOuterLeftLeg.mirror = true;
+
+
+        //bipedLeftArm.addChild(armorLeftArm);
+        bipedLeftArm.addChild(armorOuterLeftArm);
         bipedBody.addChild(armorInterBody);
         bipedBody.addChild(armorOuterBody);
-
         bipedLeftLeg.addChild(armorLeftLeg);
-
+        bipedLeftLeg.addChild(armorOuterRightLeg);
         bipedRightLeg.addChild(armorRightLeg);
-        bipedRightArm.addChild(armorRightArm);
-    }
-
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-        super.render(entity, f, f1, f2, f3, f4, f5);
-        setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-        armorRightLeg.render(f5);
-        armorLeftLeg.render(f5);
-        armorInterBody.render(f5);
-        armorRightArm.render(f5);
-        armorLeftArm.render(f5);
-        armorOuterBody.render(f5);
-        armorOuterLeftArm.render(f5);
+        bipedRightLeg.addChild(armorOuterRightLeg);
+        //bipedRightArm.addChild(armorRightArm);
     }
 }
